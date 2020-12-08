4fbfb184c83ffe5864bb8a6bfe39f911a42d4d39
angella <angella.falck@unitec.edu>
2020-12-07 18:26:06 -0600

    Added Grafos

---
+++ EjercicioGrafos/graph.cpp
@@ -0,0 +1,15 @@
+#include "graph.h"
+#include <iostream>
+
+void Graph::print() const
+{
+    for(const auto& n: nodes)
+    {
+        std::cout << n.label <<"\n";
+
+        for(const auto& t: n.trans)
+        {
+            std::cout << t.node->label<<", "<<t.weight <<"\n";
+        }
+    }
+}
---
+++ EjercicioGrafos/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "graph.h"
+
+int main()
+{
+    Graph g;
+
+    Graph::Node na("A");
+    Graph::Node nb("B");
+    Graph::Node nc("C");
+    Graph::Node nd("D");
+    Graph::Node ne("E");
+
+
+    //Node A
+    na.addTrans(&nb,100);
+    na.addTrans(&nc,10);
+    na.addTrans(&nd,5);
+
+    //Node B
+    nb.addTrans(&na,100);
+    nb.addTrans(&nc,20);
+    nb.addTrans(&ne,100);
+
+    //Node C
+    nc.addTrans(&na,10);
+    nc.addTrans(&nb,20);
+    nc.addTrans(&ne,20);
+
+    //Node D
+    nd.addTrans(&na,5);
+    nd.addTrans(&ne,20);
+
+    //Node E
+    ne.addTrans(&nb,100);
+    ne.addTrans(&nc,20);
+    ne.addTrans(&nd,20);
+
+    g.nodes.push_back(na);
+    g.nodes.push_back(nb);
+    g.nodes.push_back(nc);
+    g.nodes.push_back(nd);
+    g.nodes.push_back(ne);
+
+    g.print();
+
+}
