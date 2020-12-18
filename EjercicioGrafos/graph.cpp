646628870c73871f7458bdc7cb8b7febcd0d5b90
angella <angella.falck@unitec.edu>
2020-12-18 08:24:58 -0600

    Added Extra

---
+++ EjercicioGrafos/graph.cpp
@@ -0,0 +1,31 @@
+#include "graph.h"
+#include <iostream>
+
+void Graph::print() const
+{
+    for(const auto& n: nodes)
+    {
+        std::cout << n.label <<"\n";
+
+        for(const auto& t: n.trans)
+        {
+            std::cout << t.node->label<<", "<<t.weight <<"\n";
+        }
+    }
+}
+
+void Graph::genDotOutput() const
+{
+    std::cout<<"graph {\n";
+    for(const auto& n:nodes)
+    {
+        for(const auto& t: n.trans)
+        {
+            std::cout<<"    "
+                <<n.label<< " -- "<<t.node->label
+                <<"[ label=\"" <<t.weight<<"\""
+                <<", weight=\""<<t.weight<<"\" ];\n";
+        }
+    }
+    std::cout<<"}\n";
+}
